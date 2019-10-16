24b49f0361bf328f6d87c9b9b0212ed34e23ea9e
Jeffrey Sperling <jjs019@student.usc.edu.au>
2019-10-16 14:24:00 +1000

    added progress bar

--- app/src/main/java/android/bignerdranch/mycheckin/HelpWebPage.java
+++ app/src/main/java/android/bignerdranch/mycheckin/HelpWebPage.java
@@ -4,13 +4,18 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HelpWebPage extends AppCompatActivity {
     private WebView mWebView;
+    private ProgressBar mProgressBar;
 
     public static Intent newIntent(Context packageContext, Uri helpWebUri) {
         Intent intent = new Intent(packageContext, HelpWebPage.class);
@@ -22,6 +27,23 @@ public class HelpWebPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_helpwebpage);
+
+        mProgressBar = findViewById(R.id.progress_bar);
+        mProgressBar.setMax(100);
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView webView, int newProgress) {
+                if (newProgress == 100) {
+                    mProgressBar.setVisibility(View.GONE);
+                } else {
+                    mProgressBar.setVisibility(View.VISIBLE);
+                    mProgressBar.setProgress(newProgress);
+                }
+            }
+            public void onReceivedTitle(WebView webView, String title) {
+                getSupportActionBar().setSubtitle(title);
+            }
+        });
+
 
         mWebView = findViewById(R.id.webview);
         mWebView.getSettings().setJavaScriptEnabled(true);
--- app/src/main/java/android/bignerdranch/mycheckin/MapsActivity2.java
+++ app/src/main/java/android/bignerdranch/mycheckin/MapsActivity2.java
@@ -1,5 +1,12 @@
 package android.bignerdranch.mycheckin;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import androidx.fragment.app.FragmentActivity;
@@ -12,10 +19,14 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+
 public class MapsActivity2 extends FragmentActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
     private double mlatitude;
     private double mlongitude;
+    LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +43,87 @@ public class MapsActivity2 extends FragmentActivity implements OnMapReadyCallbac
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    Activity#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for Activity#requestPermissions for more details.
+            return;
+        }
+        if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    double mlatitude = location.getLatitude();
+                    double mlongitude = location.getLongitude();
+                    LatLng latLng = new LatLng(mlatitude, mlongitude);
+                    Geocoder geocoder = new Geocoder(getApplicationContext());
+                    try {
+                        List<Address> addressList =  geocoder.getFromLocation(mlatitude, mlongitude, 1);
+                        String str = addressList.get(0).getLocality() + ",";
+                        str += addressList.get(0).getCountryName();
+                        mMap.addMarker(new MarkerOptions().position(latLng).title(str));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10.2f));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+
+                }
+
+            });
+        } else if (locationManager.isProviderEnabled((LocationManager.GPS_PROVIDER))) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    double mlatitude = location.getLatitude();
+                    double mlongitude = location.getLongitude();
+                    LatLng latLng = new LatLng(mlatitude, mlongitude);
+                    Geocoder geocoder = new Geocoder(getApplicationContext());
+                    try {
+                        List<Address> addressList =  geocoder.getFromLocation(mlatitude, mlongitude, 1);
+                        String str = addressList.get(0).getLocality() + ",";
+                        str += addressList.get(0).getCountryName();
+                        mMap.addMarker(new MarkerOptions().position(latLng).title(str));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10.2f));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+
+                }
+            });
+        }
     }
 
 
